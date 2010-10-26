c31e53cf0d07a76d720dff993f58a06006491ea9
Mike Sassak <msassak@gmail.com>
2010-10-25 23:40:52 -0500

    Refactor Builder to use i18n#translate

--- lib/gherkin/builder.rb
+++ lib/gherkin/builder.rb
@@ -23,11 +23,11 @@ module Gherkin
     def method_missing(kw, *args, &block)
       # Need to call super or we'll get super (ha ha) weird errors
       if kw == :step
-        @elements << [:step, step_to_i18n(args[0]), args[1]]
+        @elements << [:step, translate(args[0]), args[1]]
       elsif [:given, :when, :then, :and, :but].include?(kw)
-        @elements << [:step, step_to_i18n(kw), args[0]]
+        @elements << [:step, translate(kw), args[0]]
       else # kw is for a container element
-        @elements << [kw, kw_to_i18n(kw), args[0]]
+        @elements << [kw, translate(kw), args[0]]
         instance_eval(&block) if block_given?
       end
     end
@@ -71,17 +71,8 @@ module Gherkin
       sym.to_s.split('_').map{|s| s.capitalize}.join
     end
 
-    def step_to_i18n(adverb)
-      # FIXME: I'm pretty sure this will break with langs that elide some letters (like French)
-      # and in addition it should probably be moved into the i18n class itself like say in a 
-      # translate(word) method. That would be mega-awesome.
-      kws = @i18n.keywords(adverb)
-      kws.delete("* ")
-      kws.first
-    end
-
-    def kw_to_i18n(kw)
-      @i18n.keywords(kw).first
+    def translate(word)
+      @i18n.translate(word)
     end
   end
 end
